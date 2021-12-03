60fc4d581e8401ce6882575d81037be3789b41e7
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2021-12-03 19:57:50 +0530

    Failed at implementing autocorrect but went for serpapi

--- Features/autocorrect.py
+++ Features/autocorrect.py
@@ -1,8 +1,13 @@
-from textblob import TextBlob
-s = "he is a grat person"
-print("_______original_text______")
-print(s)
-b = TextBlob(s)
-print("________Corrected text_____")
-print( str(TextBlob(s).correct()))
+# from textblob import TextBlob
+
+# s = "elon musk"
+# print("_______original_text______")
+# print(s)
+# b = TextBlob(s)
+# print("________Corrected text_____")
+# print( str(TextBlob(s).correct()))
+# from serpapi import GoogleSearch
+
+
+
 
